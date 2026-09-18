86757f7ddbbf5a8542a04a6dab6fd254fb43de60
DOIT29 <doitdiscord@gmail.com>
2026-09-19 01:43:17 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://lua.services/api/scripts/900a77b1dcbe354b5ab7aef9862abf57/loader"))()
+local GAMES = {
+    [124216119978534] = { name = "Ride A Pet",  url = "https://raw.githubusercontent.com/doitdiscord-rgb/main/refs/heads/main/rideapet.luau" },
+    [113290951185459] = { name = "Anime Dice",  url = "https://lua.services/api/scripts/900a77b1dcbe354b5ab7aef9862abf57/loader" },
+}
